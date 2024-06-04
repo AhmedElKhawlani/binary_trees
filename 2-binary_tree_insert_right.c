7a60d97a07deda2b350db28ed0059d77868da83b
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-06-04 14:45:33 -0700

    Correcting File 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Inserts a node as the right-child of another node;
+ * binary_tree_insert_right - Inserts a node as the right-child of another one;
  * @parent: Pointer to the node to insert the right-child in;
  * @value: Value to put in the new node;
  * Return: A pointer to the new node, or NULL on failure.
