b64fce8e41e0e9a20b8b4ee2129c737522b1e754
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-06-06 03:15:07 -0700

    File 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Function that finds the sibling of a node
+ * @node: Pointer to the node to find the sibling;
+ * Return: Pointer to the sibling node.
+ **/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *par;
+	binary_tree_t *nleft;
+	binary_tree_t *nright;
+
+	if (!node)
+		return (NULL);
+	par = node->parent;
+	if (!par)
+		return (NULL);
+	nleft = par->left;
+	nright = par->right;
+	if (!nleft || !nright)
+		return (NULL);
+	if (nleft == node)
+		return (nright);
+	return (nleft);
+}
